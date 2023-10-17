2dd46f90c1ef4c46e4625f59b042659542726be9
Arun Venkatesan <arvenka@corp.transunion.com>
2023-10-17 16:17:23 +0530

    added star rating

--- recipes-app/src/components/Recipe.js
+++ recipes-app/src/components/Recipe.js
@@ -1,6 +1,7 @@
 import React from "react";
 import IngredientsList from "./IngredientsList";
 import Instructions from "./Instructions";
+import StarRating from "./StarRating";
 
 function Recipe({ name = "untitled", ingredients = [], steps = [] }) {
   return (
@@ -8,6 +9,7 @@ function Recipe({ name = "untitled", ingredients = [], steps = [] }) {
       <h1>{name}</h1>
       <IngredientsList list={ingredients} />
       <Instructions title="Cooking Instructions" steps={steps} />
+      <StarRating style={{ backgroundColor: "lightblue"}} totalStars={5} onDoubleClick={e=> alert("double click")}/>
     </section>
   );
 }
